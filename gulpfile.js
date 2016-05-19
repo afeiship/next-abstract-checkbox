8774070d439797b74cb10293c7ce1f2c862ba435
afei <88603982@qq.com>
2016-05-19 13:33:17 +0800

    update file structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,43 @@
-(function () {
+/**
+ *  Welcome to your gulpfile!
+ *  The gulp tasks are splitted in several files in the gulp directory
+ *  because putting all here was really too long
+ */
 
-  var gulp = require('gulp');
-  var $ = require('gulp-load-plugins')();
+'use strict';
+
+var gulp = require('gulp');
+var path = require('path');
+var del = require('del');
+var debug = require('gulp-debug');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+
+var scripts = [
+  './src/angular/main.js',
+  './src/angular/directive/infinite-scroll.js'
+];
+
+
+gulp.task('clean', function () {
+  return del('dist');
+});
 
 
 
+gulp.task('scripts', function () {
+  return gulp.src(scripts)
+    .pipe(concat('angular-infinite-scroll.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: ".min",
+      extname: ".js"
+    }))
+    .pipe(gulp.dest('dist/js'));
+});
 
-}());
+gulp.task('default', ['clean'], function () {
+  gulp.start(['sass', 'scripts']);
+});
